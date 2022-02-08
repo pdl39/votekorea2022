f2684c41fab1f1371f1eb9fc56a7437d510a5f90
pdl39 <dhlee9111@gmail.com>
2022-02-08 08:42:39 -0500

    Update Routes

--- client/App.js
+++ client/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 import Routes from './Routes';
+import Footer from './components/Footer/Footer';
 
 
 const App = () => {
 	return (
-		<Routes />
+    <>
+      <Navbar />
+		  <Routes />
+      <Footer />
+    </>
 	);
 };
 
--- client/Routes.js
+++ client/Routes.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import KakaoLogin from './components/KakaoLogin/KakaoLogin';
 import Home from './components/Home/Home';
+import Post from './components/Post/Post';
+import Result from './components/Result/Result';
 import Fallback from './Fallback';
+import './Routes.css';
 
 const Routes = () => {
+  const dispatch = useDispatch();
+  // const auth = useSelector(state => state.auth);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect(async () => {
+	// 	dispatch(me());
+	// }, []);
+
+	// useEffect(async () => {
+	// 	setIsLoggedIn(!!auth.id);
+	// }, [auth]);
+
 	return (
-    <>
+    <div className="routes-container">
       <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/posts/:id">
+          <Post />
+        </Route>
+        <Route path="/results/:id">
+          <Result />
         </Route>
         <Route>
           <Fallback />
         </Route>
       </Switch>
-    </>
+    </div>
 	);
 };
 
