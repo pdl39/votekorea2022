caec66e0160bb9ed7a623b4ab8a20c46bcb1dfd4
pdl39 <dhlee9111@gmail.com>
2022-02-09 16:47:55 -0500

    Update error handling & isLoggedIn state logic

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getKakaoToken } from '../../store/kakaoAuth';
-import { fetchUser } from '../../store/user';
 import { fetchPost } from '../../store/post';
 import Post from '../Post/Post';
 import Result from '../Result/Result';
@@ -14,17 +13,21 @@ const Home = () => {
 
   const post = useSelector(state => state.post);
   const auth = useSelector(state => state.kakaoAuth);
-  const user = useSelector(state => state.user);
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const [isLoggedIn, setIsLoggedIn] = useState(user?.id);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(isLoggedIn);
 
   useEffect(() => {
     if (authCode) {
-      dispatch(getKakaoToken(authCode));
+      loginUser();
     }
   }, [authCode]);
+
+  useEffect(() => {
+    setIsLoggedIn(!!auth?.user?.id);
+  }, [auth?.user?.id]);
 
   useEffect(() => {
     console.log('call fetchPost dispatch')
@@ -33,12 +36,18 @@ const Home = () => {
 
   const loginUser = async () => {
     try {
-      await dispatch(getKakaoToken);
-      await dispatch(fetchUser(auth.accessToken));
-      setIsLoggedIn(user?.id);
+      const result = await dispatch(getKakaoToken(authCode));
+      console.log(result);
+      if (result.err) {
+        if (result.err.response.data.status === 403) {
+          throw new Error('로그인을 위해 대한민국에서 개통한 휴대폰 정보가 필수입니다.');
+        }
+        throw new Error('카카오 로그인 중 문제가 발생했습니다. 다시 시도해주세요.');
+      }
     }
     catch (err) {
-      window.alert('카카오 로그인 중 문제가 발생했습니다. 다시 시도해주세요.');
+      console.log({err});
+      window.alert(err);
       history.push({
         pathname: '/kakaologin',
         state: {isOpen: true}
