380afd16ce2b2dfde92d81c213fd5fd309016184
pdl39 <dhlee9111@gmail.com>
2022-02-11 07:17:51 -0500

    Add fetchChoice & removeChoice thunks and refactor

--- client/store/choice.js
+++ client/store/choice.js
@@ -1,32 +1,83 @@
 import axios from "axios";
 
+// LocalStorage Items
+const PRECHOICE_POST_ID = 'preChoicePostId';
+const PRECHOICE_ITEM_ID = 'preChoiceItemId';
+
 // Action Types
-const SUBMIT_CHOICE = 'SUBMIT_CHOICE';
+const SET_CHOICE = 'SET_CHOICE';
 
 // Action Creators
-const _submitChoice = choice => {
+const _setChoice = choice => {
   return {
-    type: SUBMIT_CHOICE,
+    type: SET_CHOICE,
     choice
   };
 };
 
 // Thunks
-export const submitChoice = (userId, itemId) => {
+export const fetchChoice = (userId, postId) => {
   return async dispatch => {
-    const { data: choice } = await axios.post(`/api/choices`, {
-      userId,
-      itemId
-    });
-    dispatch(_submitChoice(choice));
-    return choice;
+    console.log('inside fetchChoice dispatch. userId: ', userId);
+    try {
+      const { data: choice } = await axios.get(`/api/choices/?userId=${userId}&postId=${postId}`);
+      dispatch(_setChoice(choice));
+      return choice;
+    }
+    catch(err) {
+      dispatch(_setChoice({ err }));
+      return { err };
+    }
+  };
+};
+
+export const submitChoice = (userId, postId, itemId) => {
+  return async dispatch => {
+    try {
+      const { data: choice } = await axios.post(`/api/choices`, {
+        chosenItemId: itemId,
+        userId,
+        postId
+      });
+
+      // Remove pre-login choices from local storage:
+      window.localStorage.removeItem(PRECHOICE_POST_ID);
+      window.localStorage.removeItem(PRECHOICE_ITEM_ID);
+
+      dispatch(_setChoice(choice));
+      return choice;
+    }
+    catch(err) {
+      dispatch(_setChoice({ err }));
+      return { err };
+    }
+  };
+};
+
+export const removeChoice = (userId, postId) => {
+  console.log({userId, postId});
+  return async dispatch => {
+    try {
+      const { data } = await axios.delete(`/api/choices/?userId=${userId}&postId=${postId}`);
+
+      // Remove pre-login choices from local storage:
+      window.localStorage.removeItem(PRECHOICE_POST_ID);
+      window.localStorage.removeItem(PRECHOICE_ITEM_ID);
+
+      dispatch(_setChoice({}));
+      return data;
+    }
+    catch(err) {
+      dispatch(_setChoice({ err }));
+      return { err };
+    }
   };
 };
 
 // Reducer export
 export default function (state = {}, action) {
   switch (action.type) {
-    case SUBMIT_CHOICE:
+    case SET_CHOICE:
       return action.choice;
     default:
       return state;
