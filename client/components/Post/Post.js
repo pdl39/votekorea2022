e3b41dcfa4274e9f6d783f7517df5d7b777e3bff
pdl39 <dhlee9111@gmail.com>
2022-02-11 07:46:45 -0500

    Update Post - Add authenticate hook - Add choice handling logic (still working - need fixes)

--- client/components/Post/Post.js
+++ client/components/Post/Post.js
@@ -1,24 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { authenticate } from '../../store/kakaoAuth';
 import { fetchPost } from '../../store/post';
 import { fetchItems } from '../../store/items';
+import { submitChoice, fetchChoice } from '../../store/choice';
 import useStyles from './PostStyle';
 import Item from './Item/Item';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const Post = (props) => {
+	const {
+		postId: postIdProp,
+	} = props;
+
+	// LocalStorage Items Check
+	const preChoicePostId = window.localStorage.getItem('userChoiceBeforeLoginPostId');
+  const preChoiceItemId = window.localStorage.getItem('userChoiceBeforeLoginItemId');
+
 	const classes = useStyles();
 	const history = useHistory();
-	const postId = props.postId || parseInt(useParams().id, 10);
+	const postId = postIdProp || parseInt(useParams().id, 10);
+	const auth = useSelector(state => state.kakaoAuth);
 	const post = useSelector(state => state.post);
 	const items = useSelector(state => state.items);
+	const choice = useSelector(state => state.choice);
 	const dispatch = useDispatch();
+
 
 	const [isFetchPostCalled, setIsFetchPostCalled] = useState(false);
 	const [selectedItemId, setSelectedItemId] = useState(null);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	// AUTHENTICATE USER on component mount
+	useEffect(async () => {
+		const result = await dispatch(authenticate());
+		console.log('authentication dispatch was run');
+		if (result && result.user?.id) {
+			setIsLoggedIn(true);
+		}
+	}, []);
+
+	// UNMOUNT HANDLER.
+	useEffect(() => {
+		return () => {};
+	}, []);
 
 	// Handle fallback routes
 	useEffect(() => {
@@ -29,7 +56,7 @@ const Post = (props) => {
 
 	// Fetch post
 	useEffect(async () => {
-    if (postId) {
+		if (postId) {
 			await dispatch(fetchPost(postId));
 			setIsFetchPostCalled(true);
 		}
@@ -42,17 +69,56 @@ const Post = (props) => {
 		}
 	}, [postId]);
 
-	const handleSubmitChoice = (selectedItemId) => {
+	// Fetch choice
+	useEffect(() => {
+		if (isLoggedIn && postId) {
+			dispatch(fetchChoice(auth.user.id, postId));
+		}
+	}, [auth, postId]);
+
+	// Submit user's choice made before logging in (stored in local storage) after loggin in.
+	useEffect(async () => {
+		if (isLoggedIn && preChoicePostId && preChoiceItemId) {
+			const result = await dispatch(submitChoice(auth.user.id, preChoicePostId, preChoiceItemId));
+			console.log(result);
+			if (result.err && result.err.response.data === 'choice_exists') {
+				await window.alert(`선택은 한 번만 할 수 있어요! \n다시 선택 하려면 "선택 바꾸기"를 클릭하세요!`);
+			}
+			history.push(`/results/${preChoicePostId}`);
+		}
+	}, [auth, preChoicePostId, preChoiceItemId]);
+
+	const handleSubmitChoice = async (selectedItemId) => {
 		if (selectedItemId === null) {
 			window.alert('아직 선택 하지 않았어요.\n선택 해주세요!');
+			return;
 		}
 
-		dispatch(submitChoice(selectedItemId));
+		if (isLoggedIn) {
+			console.log(auth);
+			console.log(auth.user);
+			console.log(auth.user.id);
+			const result = await dispatch(fetchChoice(auth.user.id, postId));
+			if (result.choice) {
+				await window.alert(`선택은 한 번만 할 수 있어요!. \n다시 선택 하려면 결과 페이지에서 "선택 바꾸기"를 클릭하세요!`);
+				history.push(`/results/${postId}`);
+			}
+		}
+		else {
+			window.localStorage.setItem('userChoiceBeforeLoginPostId', postId);
+			window.localStorage.setItem('userChoiceBeforeLoginItemId', selectedItemId);
+			history.push({
+				pathname: '/kakaologin',
+				state: {
+					postId,
+					selectedItemId
+				}
+			});
+		}
 	}
 
 	return (
 		<div className={classes.postContainer}>
-			<CssBaseline />
 			<div className={classes.titleContainer}>
 				<Typography
 					className={classes.postTitle}
