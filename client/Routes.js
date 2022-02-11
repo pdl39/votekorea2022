65c2752612658bd974dddb216db8e9bb4b46a974
pdl39 <dhlee9111@gmail.com>
2022-02-11 07:43:15 -0500

    Update Routes - Add authenticate hook - Add Redirect to Home for /results/:id path for non-logged-in user

--- client/Routes.js
+++ client/Routes.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { authenticate } from './store/kakaoAuth';
 import KakaoLogin from './components/KakaoLogin/KakaoLogin';
 import Home from './components/Home/Home';
 import Post from './components/Post/Post';
@@ -10,13 +11,19 @@ import './Routes.css';
 
 const Routes = () => {
   const dispatch = useDispatch();
-  const auth = useSelector(state => state.kakaoAuth);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(isLoggedIn);
 
-  useEffect(() => {
-    setIsLoggedIn(!!auth.user?.id);
-  }, [auth]);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	// AUTHENTICATE USER on component mount
+	useEffect(async () => {
+    const result = await dispatch(authenticate());
+		console.log('authentication dispatch was run');
+		if (result && result.user.id) {
+      setIsLoggedIn(true);
+		}
+	}, []);
+
+  console.log(isLoggedIn);
 
 	return (
     <div className="routes-container">
@@ -34,6 +41,9 @@ const Routes = () => {
           </Route>
           <Route exact path="/posts/:id">
             <Post />
+          </Route>
+          <Route path="/results/:id">
+            <Redirect to="/" />
           </Route>
           <Route>
             <Fallback />
