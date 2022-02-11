574d2e2e21243309f0a5da7e4823d742c1eb34e7
pdl39 <dhlee9111@gmail.com>
2022-02-11 07:22:12 -0500

    Update Post model - remove postedDate

--- server/db/models/Post.js
+++ server/db/models/Post.js
@@ -2,10 +2,6 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Post = db.define('post', {
-  postedDate: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
   title: {
     type: Sequelize.STRING,
   }
