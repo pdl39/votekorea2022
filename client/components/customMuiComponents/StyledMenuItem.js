3dc313fe25bdb6ea2e6020a1077cef55d1fc7039
pdl39 <dhlee9111@gmail.com>
2022-02-12 10:41:59 -0500

    Add custom Material UI components

---
+++ client/components/customMuiComponents/StyledMenuItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
+
+const StyledMenuItem = withStyles(theme => ({
+	root: {
+		'&:focus': {
+			'& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+				color: theme.palette.primary.light,
+			},
+		},
+		height: '2em'
+	},
+}))(MenuItem);
+
+export default StyledMenuItem;
