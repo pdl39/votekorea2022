c81e7258f3368910d0e8e40dee4ba8e3fcb512d6
pdl39 <dhlee9111@gmail.com>
2022-02-13 03:10:22 -0500

    remove dbPort

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,12 @@
 const Sequelize = require('sequelize');
 
 const protocol = 'postgres';
-const dbHostName = process.env.RDS_HOSTNAME || `localhost`;
-const dbPort = process.env.RDS_PORT || 5432;
+const dbHostName = process.env.RDS_HOSTNAME || `localhost:5432`;
 const dbName = process.env.RDS_DB_NAME || 'votekorea2022';
 const dbUsername = process.env.RDS_USERNAME || '';
 const dbPassword = process.env.RDS_PASSWORD || '';
 
-const DB_URL = `${protocol}://${dbHostName}:${dbPort}/${dbName}?user=${dbUsername}&password=${dbPassword}`;
+const DB_URL = `${protocol}://${dbUsername}:${dbPassword}@${dbHostName}/${dbName}`;
 
 // const config = {
 //   logging: false
